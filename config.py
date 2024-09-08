a53462e5d4a04cd05fd5f789d5e64d7e31cb0750
Hariwar <105588706+Hariwar@users.noreply.github.com>
2024-09-08 16:40:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', '')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1520353976').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "cobrafiles_bot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "Cobrafiles_bot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
