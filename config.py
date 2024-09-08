a0380603dcde1ffa0f41356eaae71bcc4f3f9ccd
Hariwar <105588706+Hariwar@users.noreply.github.com>
2024-09-08 16:04:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ API_ID = int(environ.get("API_ID", "10103037"))
 API_HASH = environ.get("API_HASH", "29e0edd093752ae8a22cf932ff1f3ac3")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', '')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1520353976').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "cobrafiles_bot") # without @
 PORT = environ.get("PORT", "8080")
